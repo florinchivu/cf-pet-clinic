624d4cd8e864fd748a9cf5e2eab8356ae280aee3
Florin Chivu <florin.chivu@metrosystems.net>
2018-10-05 19:05:17 +0300

    Initial Project Add

---
+++ src/main/java/chivu/florin/cfpetclinic/CfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package chivu.florin.cfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfPetClinicApplication.class, args);
+	}
+}
