3964086ee19a42329203ff50e8eb79d59b2f7ee8
Florin Chivu <florin.chivu@metrosystems.net>
2018-11-10 08:24:42 +0200

    closes #10

--- pet-clinic-data/src/main/java/chivu/florin/cfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/chivu/florin/cfpetclinic/services/OwnerService.java
@@ -2,15 +2,9 @@ package chivu.florin.cfpetclinic.services;
 
 import chivu.florin.cfpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long> {
 
   Owner findByLastName(String lastName);
 
-  Owner findbyId(Long id);
-
-  Owner save(Owner owner);
-
-  Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/chivu/florin/cfpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/chivu/florin/cfpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package chivu.florin.cfpetclinic.services;
 
 import chivu.florin.cfpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-  Pet findbyId(Long id);
-
-  Pet save(Pet pet);
-
-  Set<Pet> findAll();
+public interface PetService extends CrudService<Pet , Long>{
 }
